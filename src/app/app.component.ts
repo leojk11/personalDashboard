3aa4371eddce556624aed4dcf9f93801cc52c668
Leonid Jakimovski <ljakimovski211@yahoo.com>
2021-04-06 16:22:43 +0200

    added animations
    implemented fade animation when the route changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
+import { animate, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routeAnim', [
+      transition('* => *', [
+        query(':enter', [
+          style({
+            opacity: 0,
+            display: 'block',
+            height: '100%'
+          }),
+          animate(1000, style({
+            opacity: 1
+          }))
+        ], { optional: true })
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'personal-dashboard';
+
+  prepareRoute(outlet: RouterOutlet) {
+    if (outlet.isActivated) {return outlet.activatedRoute.snapshot.url}
+    else {
+      return 
+    }
+  }
+
 }

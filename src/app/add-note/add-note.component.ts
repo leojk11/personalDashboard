cfce82e25c27baf11a60f3b27e757a46837067db
Leonid Jakimovski <ljakimovski211@yahoo.com>
2021-04-07 12:11:01 +0200

    Added add new note functionality

--- src/app/add-note/add-note.component.ts
+++ src/app/add-note/add-note.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+// router
+import { Router } from '@angular/router';
+
+// angular forms
+import { NgForm } from '@angular/forms';
+
+// note model
+import { Note } from '../shared/note.model';
+
+// note service
+import { NoteService } from '../shared/note.service';
+
 @Component({
   selector: 'app-add-note',
   templateUrl: './add-note.component.html',
@@ -7,9 +19,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNoteComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private noteService: NoteService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  // when form get submitted this module is getting called
+  onFormSubmit(form: NgForm) {
+    // create new note of type Note
+    const note = new Note(form.value.title, form.value.content);
+
+    // add the new create note to the note service array
+    this.noteService.addNote(note);
+
+    // when the new note is created, navite the user to the notes route
+    this.router.navigateByUrl('/notes');
   }
 
 }

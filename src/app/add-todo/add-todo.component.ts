0e480df6a0f484bae3e6bc376352ba16b5b1ab9e
Leonid Jakimovski <ljakimovski211@yahoo.com>
2021-04-08 16:59:31 +0200

    Added notification in the add-todo component

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
+// notification service
+import { NotificationService } from '../shared/notification.service';
+
 // todo model
 import { Todo } from '../shared/todo.model';
 
@@ -19,23 +22,32 @@ export class AddTodoComponent implements OnInit {
 
   constructor(
     private todoService: TodoService,
-    private router: Router
+    private router: Router,
+    private notificationService: NotificationService
   ) { }
 
   ngOnInit(): void {
   }
 
+  // on form submit function
   onFormSubmit(form: NgForm) {
+    // if form is invalid show errors and return
     if(form.invalid) {
       this.showValidationErrors = true;
 
       return;
     };
 
+    // create new todo
     const todo = new Todo(form.value.text);
 
+    // add new todo to the todos list
     this.todoService.addTodo(todo);
 
+    // set notification when the todo is added
+    this.notificationService.show('New todo has been added', 3000);
+
+    // reroute when new todo has been added
     this.router.navigateByUrl('/todos');
   }
 
